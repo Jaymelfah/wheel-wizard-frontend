6e7b45d9f38324d16f33d1fd3dbc98fc44db921f
KareemWilson <kareemmagdywilson@gmail.com>
2023-03-02 14:48:05 +0200

    Fix linters Errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="app">
       <div className="d-flex flex-row">
         <SideBar />
-        <div className="bg-primary">Hello</div>
       </div>
     </div>
   );
