571da16629c832e3566af3054a387e3cb4b825a8
KareemWilson <kareemmagdywilson@gmail.com>
2023-03-02 12:05:41 +0200

    Fix Bug

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import store from './redux/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
