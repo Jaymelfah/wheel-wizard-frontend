311b7a568574d58efd20434133b86366d7621a4a
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-03-06 16:33:39 +0000

    Create car reducer

---
+++ src/redux/car/car.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const Url = 'http://localhost:3000/api/v1/cars';
+
+const initialState = {
+  status: null,
+};
+
+export const createCar = createAsyncThunk(
+  'create/createCar',
+  async (payload, thunkAPI) => {
+    try {
+      const response = await axios.post(Url, payload, {
+        car: payload,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const createCarSlice = createSlice({
+  name: 'newCar',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCar.pending, (state) => {
+        const IsPending = state;
+        IsPending.status = 'pending';
+      })
+      .addCase(createCar.fulfilled, (state) => {
+        const IsFulfilled = state;
+        IsFulfilled.status = 'fulfilled';
+      })
+      .addCase(createCar.rejected, (state) => {
+        const IsRejected = state;
+        IsRejected.status = 'rejected';
+      });
+  },
+});
+
+export default createCarSlice.reducer;
