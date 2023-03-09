16a231ee8283905bd6e722725da1bedb448da59d
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-03-09 23:46:37 +0000

    Add links to car card

--- src/pages/cars/CarCard/CarCard.jsx
+++ src/pages/cars/CarCard/CarCard.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './car-card.css'
 
 function CarCard({ car }) {
+ 
   return (
+    <Link to={`/details/${car.id}`}>
     <div className="car-card d-flex flex-column p-5">
       <img src={car.image_url} className='car-image d-flex flex-column' />
       <h2>{car.name}</h2>
       <p style={{ color: 'rgb(182 183 184)' }}>{car.description}</p>
     </div>
+    </Link>
   );
 }
 
