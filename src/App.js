a103d1ea5333270ca477c3743924a612d126f9ea
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-03-09 12:31:24 +0000

    Update Routes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { Routes, Route } from 'react-router-dom';
 import SideBar from './components/sidebar/SideBar';
 import Cars from './pages/cars/Cars';
 import AddCar from './components/addCar/AddCar';
+import RemoveCar from './components/deleteCar/deletecar';
 
 function App() {
   return (
     <div className="app">
       <div className="main-container d-flex flex-row">
         <SideBar />
-        <Cars />
       </div>
+      <Routes>
+        <Route path="/" element={<Cars />} />
+      </Routes>
+      <Routes>
+        <Route path="/delete-car" element={<RemoveCar />} />
+      </Routes>
       <Routes>
         <Route path="/add-car" element={<AddCar />} />
       </Routes>
