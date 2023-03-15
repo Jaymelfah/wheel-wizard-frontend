131849e5f270082364bdb52509f405655072e91a
KareemWilson <kareemmagdywilson@gmail.com>
2023-03-15 12:25:46 +0200

    test deploy

--- src/APIs/user.js
+++ src/APIs/user.js
@@ -4,7 +4,7 @@ import { URL } from '../constants';
 
 export const createAccount = async (reqBody) => {
   try {
-    const response = await axios.post(`${URL}/users`, { user: reqBody });
+    const response = await axios.post(`${URL}users`, { user: reqBody });
     toast.success('Signed Up Successfully');
     return response.data;
   } catch (error) {
